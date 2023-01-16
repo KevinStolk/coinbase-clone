3f47ed0ac7825eb30db1618e0fcca29488d4b07a
KevinStolk <stolkkevin02@gmail.com>
2023-01-16 11:34:12 +0100

    CoinPage is now dynamic based on url, added prettier and ts interfaces

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,12 @@ import SignIn from './pages/auth/SignIn'
 import SignUp from './pages/auth/SignUp'
 import Home from './pages/Home'
 import axios from 'axios'
+import CoinPage from './pages/CoinPage'
+import Footer from './components/Footer'
 
 function App() {
-    const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=10&page=1&sparkline=true'
+    const url =
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=10&page=1&sparkline=true'
 
     const [coins, setCoins] = useState([])
 
@@ -23,11 +26,15 @@ function App() {
         <ThemeProvider>
             <Navbar />
             <Routes>
-                <Route path='/' element={<Home {...{ coins }} />} />
-                <Route path='/signin' element={<SignIn />} />
-                <Route path='/signup' element={<SignUp />} />
-                <Route path='/accoun' element={<Account />} />
+                <Route path="/" element={<Home {...{ coins }} />} />
+                <Route path="/signin" element={<SignIn />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/account" element={<Account />} />
+                <Route path="/coin/:id" element={<CoinPage />}>
+                    <Route path=":id" />
+                </Route>
             </Routes>
+            <Footer />
         </ThemeProvider>
     )
 }
--- src/components/CoinItem.tsx
+++ src/components/CoinItem.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import { AiOutlineStar } from 'react-icons/ai'
-import { Sparklines, SparklinesLine } from 'react-sparklines-typescript'
+import React from "react";
+import { AiOutlineStar } from "react-icons/ai";
+import { Sparklines, SparklinesLine } from "react-sparklines-typescript";
+import { Link } from "react-router-dom";
 
 interface ICoins {
     id: string;
@@ -13,35 +14,55 @@ interface ICoins {
     market_cap: number;
     market_cap_rank: number;
     sparkline_in_7d: {
-        price: number[]
-    }
+        price: number[];
+    };
 }
 
-const CoinItem: React.FC<{coin: ICoins}> = ({ coin }) => {
+const CoinItem: React.FC<{ coin: ICoins }> = ({ coin }) => {
     return (
-        <tr className='h-[80px] border-b overflow-hidden' key={coin.id}>
-            <td><AiOutlineStar /></td>
+        <tr className="h-[80px] border-b overflow-hidden" key={coin.id}>
+            <td>
+                <AiOutlineStar />
+            </td>
             <td>{coin.market_cap_rank}</td>
             <td>
-                <div className='flex items-center'>
-                    <img className='w-6 mr-2 rounded-full' src={coin.image} alt={coin.id} />
-                    <p className='hidden sm:table-cell'>{coin.name}</p>
-                </div>
+                <Link to={`/coin/${coin.id}`}>
+                    <div className="flex items-center">
+                        <img
+                            className="w-6 mr-2 rounded-full"
+                            src={coin.image}
+                            alt={coin.id}
+                        />
+                        <p className="hidden sm:table-cell">{coin.name}</p>
+                    </div>
+                </Link>
             </td>
             <td>{coin.symbol.toUpperCase()}</td>
             <td>€{coin.current_price.toLocaleString()}</td>
             <td>
-            {coin.price_change_percentage_24h >= 0 ? (<p className='text-green-600'>{coin.price_change_percentage_24h.toFixed(2)}%</p>) : (<p className='text-red-600'>{coin.price_change_percentage_24h.toFixed(2)}%</p>)}
+                {coin.price_change_percentage_24h >= 0 ? (
+                    <p className="text-green-600">
+                        {coin.price_change_percentage_24h.toFixed(2)}%
+                    </p>
+                ) : (
+                    <p className="text-red-600">
+                        {coin.price_change_percentage_24h.toFixed(2)}%
+                    </p>
+                )}
             </td>
-            <td className='w-[180px] hidden md:table-cell'>€{coin.total_volume.toLocaleString()}</td>
-            <td className='w-[180px] hidden sm:table-cell'>€{coin.market_cap.toLocaleString()}</td>
+            <td className="w-[180px] hidden md:table-cell">
+                €{coin.total_volume.toLocaleString()}
+            </td>
+            <td className="w-[180px] hidden sm:table-cell">
+                €{coin.market_cap.toLocaleString()}
+            </td>
             <td>
-                <Sparklines data={coin.sparkline_in_7d.price} >
-                    <SparklinesLine color='teal' ></SparklinesLine>
+                <Sparklines data={coin.sparkline_in_7d.price}>
+                    <SparklinesLine color="teal"></SparklinesLine>
                 </Sparklines>
             </td>
         </tr>
-    )
-}
+    );
+};
 
 export default CoinItem;
