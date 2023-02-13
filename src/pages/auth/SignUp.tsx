4d42caef912bf3fd6316e4c57263bb17f4c4a47e
KevinStolk <stolkkevin02@gmail.com>
2023-02-13 12:14:42 +0100

    Added form validation and error fixes with building

--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -2,23 +2,44 @@ import React, { useState } from 'react'
 import { AiFillLock, AiOutlineMail } from 'react-icons/ai'
 import { Link, useNavigate } from 'react-router-dom'
 import { UserAuth } from '../../context/AuthContext'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+
+interface SignUpFormData {
+    email: string
+    password: string
+    confirmPassword: string
+}
 
 const SignUp = () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
     const [error, setError] = useState('')
-
     const navigate = useNavigate()
     const { signUp } = UserAuth()
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        setError('')
+    const schema = yup.object().shape({
+        email: yup.string().email().required('Email is required'),
+        password: yup.string().min(4).max(20).required('Password is required'),
+        confirmPassword: yup
+            .string()
+            .oneOf([yup.ref('password'), null], "Passwords Don't Match")
+            .required('Password is required'),
+    })
 
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<SignUpFormData>({
+        resolver: yupResolver(schema),
+    })
+
+    const onSubmit = async (data: SignUpFormData) => {
+        setError('')
         try {
-            await signUp(email, password)
-            navigate('/account')
-        } catch (err) {
+            await signUp(data.email, data.password)
+            navigate('/')
+        } catch (err: any) {
             setError(err.message)
             console.error(err.message)
         }
@@ -28,31 +49,37 @@ const SignUp = () => {
         <div>
             <div className="max-w-[400px] mx-auto min-h-[600px] px-4 py-20">
                 <h1 className="text-2xl font-bold">Sign Up</h1>
-                {error ? <p className="bg-red-300 p-3 my-2">{error}</p> : null}
-                <form onSubmit={handleSubmit}>
+                {error ? <p className="bg-red-500 p-3 my-2">{error}</p> : null}
+                <form onSubmit={handleSubmit(onSubmit)}>
                     <div className="my-4">
                         <label>Email</label>
                         <div className="my-2 w-full relative rounded-md shadow-xl">
                             <input
-                                onChange={(e) => setEmail(e.target.value)}
                                 className="w-full p-2 bg-primary border border-input rounded-md"
                                 type="email"
                                 placeholder="Enter your email..."
+                                {...register('email')}
                             />
                             <AiOutlineMail className="absolute right-2 top-3 text-gray-400" />
                         </div>
+                        <p className="text-red-500">
+                            {errors.email?.message?.toString()}
+                        </p>
                     </div>
                     <div className="my-4">
                         <label>Password</label>
                         <div className="my-2 w-full relative rounded-md shadow-xl">
                             <input
-                                onChange={(e) => setPassword(e.target.value)}
                                 className="w-full p-2 bg-primary border border-input rounded-md"
                                 type="password"
                                 placeholder="Enter your password..."
+                                {...register('password')}
                             />
                             <AiFillLock className="absolute right-2 top-3 text-gray-400" />
                         </div>
+                        <p className="text-red-500">
+                            {errors.password?.message?.toString()}
+                        </p>
                     </div>
                     <button className="w-full my-2 p-3 bg-button text-btnText rounded-md shadow-xl">
                         Sign up
